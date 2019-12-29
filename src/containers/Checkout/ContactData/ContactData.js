614e5a334b6d4d76898a66f8009db1544de9c843
Alex <ergo.picault@gmail.com>
2019-12-29 11:54:41 +0100

    new Input react element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state= {
@@ -44,10 +45,10 @@ class ContactData extends Component {
 
     render () {
         let form = <form>
-            <input className={classes.Input} type='text' name='name' placeholder='your name' />
-            <input className={classes.Input} type='email' name='email' placeholder='your email' />
-            <input className={classes.Input} type='text' name='street' placeholder='your street' />
-            <input className={classes.Input} type='text' name='postalCode' placeholder='your postal code' />
+            <Input inputtype='input' type='text' name='name' placeholder='your name' />
+            <Input inputtype='input' type='email' name='email' placeholder='your email' />
+            <Input inputtype='input' type='text' name='street' placeholder='your street' />
+            <Input inputtype='input' type='text' name='postalCode' placeholder='your postal code' />
         </form>;
         if (this.state.loading) {
             form = <Spinner />
